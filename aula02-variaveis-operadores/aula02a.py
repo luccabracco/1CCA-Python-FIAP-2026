064479d829649121ce79059d11a0093504106536
luccabracco <luccabraccoferreira@gmail.com>
2026-03-20 11:31:47 -0300

    Aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,34 @@
+print('Olá Mundo')
+print(7+4)
+print('7 + 4')
+print('7'+'4') # Concatenação de strings
+
+# comentario de 1 linha
+'''
+Comentarios de 
+multiplas
+linhas
+'''
+
+# variaveis
+nome="Lucca" # str
+idade=21 # int
+peso=93 # float
+
+print(nome,idade,peso)
+print(f"Oiiii {nome}!!!")
+
+# input - Simulação de um forms no CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = int(input("Digite seu peso: "))
+
+print(nome,idade,peso)
+print(idade + 1)
+print(peso - 8)
+
+nova_idade = idade + 1
+novo_peso = peso - 8
+
+print(nova_idade)
+print(novo_peso)
---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,13 @@
+# Desafio 1
+
+nome = input("Digite seu nome: ")
+
+print(f"Seja Bem Vindo {nome}!")
+
+# Desafio 2
+
+dia = int(input("Digite o dia do seu aniversário: "))
+mes = input("Digite o mes do seu aniversário: ")
+ano = int(input("Digite o ano que você nasceu: "))
+
+print(f"Seu aniversario é dia {dia} de {mes}, e você nasceu no ano de {ano}!")
