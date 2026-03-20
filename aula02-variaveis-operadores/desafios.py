ad76070e16b306e1c9571f471bf8ad0e47a280f0
luccabracco <luccabraccoferreira@gmail.com>
2026-03-20 11:37:57 -0300

    Finalização dos Desafios

--- aula02-variaveis-operadores/desafios.py
+++ aula02-variaveis-operadores/desafios.py
@@ -10,4 +10,13 @@ dia = int(input("Digite o dia do seu aniversário: "))
 mes = input("Digite o mes do seu aniversário: ")
 ano = int(input("Digite o ano que você nasceu: "))
 
-print(f"Seu aniversario é dia {dia} de {mes}, e você nasceu no ano de {ano}!")
+print(f"Seu aniversario é dia {dia} de {mes} no ano de {ano}!")
+
+# Desafio 3
+
+numeroa = int(input("Escolha um número: "))
+numerob = int(input("Escolha outro número: "))
+
+soma = (numeroa + numerob)
+
+print(f"A soma de {numeroa} + {numerob} é de {soma}!")
