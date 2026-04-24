ddc82b3a1a09b2c5a46ce5733ebe2c047c8aba5b
luccabracco <luccabraccoferreira@gmail.com>
2026-04-24 11:35:22 -0300

    atv 3.1

--- aula04-repeticao/atividade03.py
+++ aula04-repeticao/atividade03.py
@@ -3,3 +3,8 @@ qtd_music = int(input("Digite a quantidade de musica que voce tem na sua playlis
 for i in range(qtd_music):
     print(f"Musica {i}")
 
+# repeticoes encadeadas
+
+for l in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"l = {l}, j = {j}")
